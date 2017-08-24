4e31304c5e2624f6f62e6bf09462a1d77b05de90
Prasoon Mehra <prasoonmehra14@gmail.com>
2017-08-24 07:36:17 +0000

    Update leap.c
    Fix Logic

--- leap.c
+++ leap.c
@@ -2,17 +2,19 @@
 #include <stdio.h>
 int main()
 {
-        int y;
-        printf ("\nEnter the Year=");
-        scanf ("%d" ,&y);
-
-        if (y%4==0)
-         {
-          printf ("%d is a leap year\n" ,y);
-         }
-
+int y;
+printf ("\nEnter the Year=");
+scanf ("%d" ,&y);
+        if (y%4==0&&y%100!=0)
+        {
+        printf ("%d is a leap year\n" ,y);
+        }
+        else if (y%4==0&&y%100==0&&y%400==0)
+        {
+        printf ("%d is a leap year\n" ,y);
+        }
         else
-         {
-          printf ("%d is not a leap year\n" ,y);
-         }
+        {
+        printf ("%d is not a leap year\n" ,y);
+        }
 }
